c4459d1d1a3d1f119f5a81d623af3ff83743550f
Lupercal13 <hpayne13@gmail.com>
2023-05-05 22:26:01 +0900

    added items to portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,36 @@
-import { projects } from "../constants"
+import { projects } from "../constants";
 
 const Portfolio = () => {
   return (
-    <div id='portfolio' className='bg-gray-300 pt-32'>
-        <div>
-            <div className="pb-16 text-center">
-                <h1 className='text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-purple-700  text-center sm:pl-20'>A Collection Of My Works</h1>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 mx-8">
-                {projects.map((item) => (
-                    <div className="card w-96 bg-base-100 shadow-xl" key={item.id}>
-                        <figure>
-
-                        <img src={item.image} alt="placeholder" className="pt-8 px-4"/>
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title ">{item.title}</h2>
-                            <p className="pb-4">{item.description}</p>
-                            <div className="card-actions justify-center gap-5">
-                                <button className="btn btn-primary">Live Demo</button>
-                                <button className="btn btn-primary">View on Github</button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                ))}
-            </div>
+    <div id="portfolio" className="bg-gray-300 pt-32">
+      <div>
+        <div className="pb-16 text-center">
+          <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-purple-700  text-center sm:pl-20">
+            A Collection Of My Works
+          </h1>
         </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 mx-8">
+          {projects.map((item) => (
+            <div className="card w-96 bg-base-100 shadow-xl" key={item.id}>
+              <figure>
+                <img src={item.image} alt="placeholder" className="pt-8 px-4" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title ">{item.title}</h2>
+                <p className="pb-4">{item.description}</p>
+                <div className="card-actions justify-center gap-5">
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    <button className="btn btn-primary">Live Demo</button>
+                  </a>
+                  <button className="btn btn-primary">View on Github</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
-
+export default Portfolio;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,9 @@
 import placeholder from "../assets/placeholder.png";
 import YTClone from '../assets/YTClone.png'
+import todoList from '../assets/todo-list(1).png'
+import blog from '../assets/blog.png'
+import halcyon from '../assets/halcyon-kleos.png'
+import weather from '../assets/weather-app.png'
 export const navLinks = [
   {
     id: "home",
@@ -26,40 +30,45 @@ export const projects = [
     description:
       "A clone of YouTube built using Create React App and Material UI along with the YouTube API and hosted on Vercel",
     image: YTClone,
+    link: 'https://yt-clone-nu.vercel.app/',
   },
   {
     id: 2,
-    title: "Sample Project 2",
+    title: "Simple ToDo List",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
-    image: placeholder,
+      "This is a simple todo list app created using react. The app saves information in local storage and includes a color palette picker and dark/light mode toggler.",
+    image: todoList,
+    link: 'https://todo-react-theme.netlify.app'
   },
   {
     id: 3,
-    title: "Sample Project 3",
+    title: "Personal Blog",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
-    image: placeholder,
+      "A personal blog created using NextJS and graphCMS.",
+    image: blog,
+    link: 'https://my-blog-lupercal13.vercel.app/'
   },
   {
     id: 4,
-    title: "Sample Project 4",
+    title: "Customer Website",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
-    image: placeholder,
+      "This is a website created for a music producer client. They sent the designs and specifications to be fulfilled. Created using Vite and React. Uses the Twitter API to pull most recent posts.",
+    image: halcyon,
+    link: 'https://halcyon-kleos-v2.vercel.app'
   },
   {
     id: 5,
-    title: "Sample Project 5",
+    title: "Weather App",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
-    image: placeholder,
+      "This weather app was built using React and takes advantage of the OpenWeather API to get the current weather information. ",
+    image: weather,
+    link: 'https://react-weather-nine-iota.vercel.app'
   },
-  {
-    id: 6,
-    title: "Sample Project 6",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
-    image: placeholder,
-  },
+//   {
+//     id: 6,
+//     title: "Sample Project 6",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam fugit adipisci alias sed architecto.",
+//     image: placeholder,
+//   },
 ];
